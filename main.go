1f02d0317e241fec9f9e49cfdd94dee354aab066
Casey Manus <cmanus@xoi.io>
2018-03-29 15:46:23 -0500

    changing greeting to test CD

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
+		w.Write([]byte("Hello Cruel World of Continuous Delivery!"))
 	})
 
 	http.ListenAndServe(":80", nil)
